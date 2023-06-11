172535dc55bb52121ca45724596999d153aae3f0
ponatosik <ponatosik@gmail.com>
2023-06-11 22:30:35 +0300

    Add more actions to WorkspacesController

--- Realtime-ToDo-Web-API/Controllers/WorkspacesController.cs
+++ Realtime-ToDo-Web-API/Controllers/WorkspacesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Realtime_ToDo_Web_API.Models;
 using Realtime_ToDo_Web_API.Services;
+using Realtime_ToDo_Web_API.Services.SignalR;
 
 namespace Realtime_ToDo_Web_API.Controllers;
 
@@ -9,20 +10,57 @@ namespace Realtime_ToDo_Web_API.Controllers;
 public class WorkspacesController : ControllerBase
 {
     private readonly TodoListService _todoListService;
-    public WorkspacesController(TodoListService todoListService)
+    private readonly ConnectionManager _connectionManager;
+
+    public WorkspacesController(TodoListService todoListService, ConnectionManager connectionManager)
     {
         _todoListService = todoListService;
+        _connectionManager = connectionManager;
     }
 
-    [HttpGet(Name = "GetWorkspaces")]
+    [HttpGet]
     public IEnumerable<WorkspaceInfo> Get()
     {
         return _todoListService.GetWorkspacesInfo();
     }
 
-    [HttpPut(Name = "PutWorkspaces")]
-    public async Task<WorkspaceInfo> PutAsync(string worskspaceName)
+    [HttpGet("{workspaceId}")]
+    public ActionResult<WorkspaceInfo> Get(int workspaceId)
     {
-        return await _todoListService.AddWorkspace(worskspaceName);
+        WorkspaceInfo? workspace = _todoListService.GetWorkspaceInfo(workspaceId);
+        if (workspace == null) return NotFound($"Workspace with id {workspaceId} not found");
+        return Ok(workspace!);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<WorkspaceInfo>> Post(string worskspaceName)
+    {
+        WorkspaceInfo createdWorkspace = await _todoListService.AddWorkspace(worskspaceName);
+        var routeValues = new
+        {
+            workspaceId = createdWorkspace.Id,
+        };
+        return CreatedAtAction(nameof(Get), routeValues, createdWorkspace);
+    }
+
+    [HttpPatch("{workspaceId}")]
+    public async Task<ActionResult<WorkspaceInfo>> Patch(int workspaceId, string newWorkspaceName)
+    {
+        WorkspaceInfo? updatedWorkspace = await _todoListService.UpdateWorkspaceInfo(workspaceId, workspace =>
+        {
+            workspace.Name = newWorkspaceName;
+        });
+        if (updatedWorkspace == null) return NotFound($"Workspace with id {workspaceId} not found");
+        return Ok(updatedWorkspace);
+    }
+
+    [HttpDelete("{workspaceId}")]
+    public async Task<ActionResult<WorkspaceInfo>> Delete(int workspaceId)
+    {
+        WorkspaceInfo? deletedWorkspace = await _todoListService.DeleteWorkspace(workspaceId);
+        if (deletedWorkspace == null) return NotFound($"Workspace with id {workspaceId} not found");
+        if (_connectionManager.GetConnectedUsers(workspaceId) > 0)
+            return Conflict($"Workspace with id {workspaceId} is currently used by other users. Try again later");
+        return Ok(deletedWorkspace);
     }
 }
--- Realtime-ToDo-Web-API/Models/WorkspaceInfo.cs
+++ Realtime-ToDo-Web-API/Models/WorkspaceInfo.cs
@@ -3,7 +3,7 @@
 public class WorkspaceInfo
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = "New workspace";
     public int TaskCount { get; set; }
 
     public WorkspaceInfo(Workspace workspace, int? taskCount)
@@ -13,4 +13,5 @@ public class WorkspaceInfo
         TaskCount = taskCount ?? 0;
     }
     public WorkspaceInfo(Workspace workspace) : this(workspace, workspace.Tasks?.Count) { }
+    public WorkspaceInfo() { }
 }
