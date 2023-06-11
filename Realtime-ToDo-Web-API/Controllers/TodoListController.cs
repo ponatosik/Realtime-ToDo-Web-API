c20ad8a547691687810583a2b90e858c52b3e5fe
ponatosik <ponatosik@gmail.com>
2023-06-11 20:33:56 +0300

    Add more actions to TodoListController

--- Realtime-ToDo-Web-API/Controllers/TodoListController.cs
+++ Realtime-ToDo-Web-API/Controllers/TodoListController.cs
@@ -15,7 +15,7 @@ public class TodoListController : ControllerBase
         _todoListService = todoListService;
     }
 
-    [HttpGet("{workspaceId}", Name = "GetTodoListworkspaceId")]
+    [HttpGet("{workspaceId}")]
     public ActionResult<IEnumerable<TodoTask>> Get(int workspaceId)
     {
         IEnumerable<TodoTask>? tasks = _todoListService.GetWorkspaceTasks(workspaceId)?.OrderBy(task => task.Order);
@@ -23,11 +23,44 @@ public class TodoListController : ControllerBase
         return Ok(tasks!);
     }
 
-    [HttpPut("{workspaceId}", Name = "PutTodoListByworkspaceId")]
-    public async Task<ActionResult<TodoTask>> Put(TodoTask task, int workspaceId)
+    [HttpGet("{workspaceId}/{taskId}")]
+    public ActionResult<TodoTask> Get(int workspaceId, int taskId)
+    {
+        TodoTask? task = _todoListService.GetTask(workspaceId, taskId);
+        if (task == null) return NotFound($"Task with id {taskId} not found in workspace with id {workspaceId}");
+        return Ok(task!);
+    }
+
+    [HttpPost("{workspaceId}")]
+    public async Task<ActionResult<TodoTask>> Post(TodoTask task, int workspaceId)
     {
         TodoTask? createdTask = await _todoListService.AddTask(workspaceId, task);
         if (createdTask == null) return NotFound($"Workspace with id {workspaceId} not found");
-        return Ok(createdTask);
+        var routeValues = new
+        {
+            workspaceId = workspaceId,
+            taskId = createdTask.Id
+        };
+        return CreatedAtAction(nameof(Get), routeValues, createdTask);
+    }
+
+    [HttpDelete("{workspaceId}/{taskId}")]
+    public async Task<ActionResult<TodoTask>> Delete(TodoTask task, int workspaceId, int taskId)
+    {
+        TodoTask? deletedTask = await _todoListService.DeleteTask(workspaceId, taskId);
+        if (deletedTask == null) return NotFound($"Task with id {taskId} not found in workspace with id {workspaceId}");
+        return Ok(deletedTask);
+    }
+
+    [HttpPut("{workspaceId}")]
+    public async Task<ActionResult<TodoTask>> Put(TodoTask newTask, int workspaceId)
+    {
+        var taskId = newTask.Id;
+        TodoTask? updatedTask = await _todoListService.UpdateTask(workspaceId, taskId, task =>
+        {
+            task = newTask;
+        });
+        if (updatedTask == null) return NotFound($"Task with id {taskId} not found in workspace with id {workspaceId}");
+        return Ok(updatedTask);
     }
 }
