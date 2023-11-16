c91bf8b03c816105c19e37571b6ec2a2700cd053
ponatosik <ponatosik@gmail.com>
2023-11-16 10:48:04 +0200

    fix: configure dynamic connection string for Azure MySQL In App

--- Realtime-ToDo-Web-API/src/Startup/DatabaseSetup.cs
+++ Realtime-ToDo-Web-API/src/Startup/DatabaseSetup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Realtime_ToDo_Web_API.Data;
+using System.Data.Common;
+using System.Text.RegularExpressions;
 
 namespace Realtime_ToDo_Web_API.Startup;
 
@@ -9,10 +11,34 @@ public static class DatabaseSetup
 	{
 		if (builder.Environment.IsDevelopment())
 			builder.Services.ConfigureInMemoryDatabase();
-		else
+		else if (builder.Configuration.GetConnectionString("TodoListDB") is not null)
 			builder.Services.ConfigureMySqlDatabase(builder.Configuration.GetConnectionString("TodoListDB")!);
+		else if (Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb") is not null)
+			builder.Services.ConfigureAzureMySqlInAppDatabase(Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb")!);
 
 		return builder;
+	}
+
+	private static IServiceCollection ConfigureAzureMySqlInAppDatabase(this IServiceCollection services, string connectionString)
+	{
+		DbConnectionStringBuilder inDbConnectionStringBuilder = new DbConnectionStringBuilder();
+		inDbConnectionStringBuilder.ConnectionString = connectionString;
+
+		String database = (string)inDbConnectionStringBuilder["Database"];
+		String sourceIp = ((string)inDbConnectionStringBuilder["Data Source"])[..^6];
+		String sourcePort = ((string)inDbConnectionStringBuilder["Data Source"])[^5..];
+		String user = (string)inDbConnectionStringBuilder["User Id"];
+		String password = (string)inDbConnectionStringBuilder["Password"];
+
+		DbConnectionStringBuilder outDbConnectionStringBuilder = new DbConnectionStringBuilder();
+
+		outDbConnectionStringBuilder.Add("Data Source", sourceIp);
+		outDbConnectionStringBuilder.Add("Port", sourcePort);
+		outDbConnectionStringBuilder.Add("Database", database);
+		outDbConnectionStringBuilder.Add("User Id", user);
+		outDbConnectionStringBuilder.Add("Password", password);
+
+		return ConfigureMySqlDatabase(services, outDbConnectionStringBuilder.ConnectionString);
 	}
 
 	private static IServiceCollection ConfigureMySqlDatabase(this IServiceCollection services, string connectionString) 
